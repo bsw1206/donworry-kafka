538511f4c94be6f34e788f4732a39847d150b31b
bsw1206 <toysw1206@gmail.com>
2026-05-14 14:15:24 +0900

    feat: update consumer_stock.py

---
+++ products/management/commands/consume_stock.py
@@ -0,0 +1,23 @@
+import json
+from django.core.management.base import BaseCommand
+from kafka import KafkaConsumer
+
+class Command(BaseCommand):
+    help = 'Kafka에서 주식 데이터를 가져옵니다.'
+
+    def handle(self, *args, **options):
+        consumer = KafkaConsumer(
+            'stock-data', # 구독할 토픽 이름
+            bootstrap_servers=['127.0.0.1:9092'],
+            auto_offset_reset='earliest', # 처음부터 읽기
+            enable_auto_commit=True,
+            group_id='django-group-1', # 컨슈머 그룹
+            value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+        )
+
+        self.stdout.write(self.style.SUCCESS('데이터 수신 대기 중...'))
+
+        for message in consumer:
+            data = message.value
+            # 🎯 여기서 가져온 데이터를 DB에 저장하거나 가공하면 됩니다!
+            self.stdout.write(self.style.SUCCESS(f"받은 데이터: {data}"))
