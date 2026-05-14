65642500342e39fe67affcf5fb13d51c97bd66ec
bsw1206 <toysw1206@gmail.com>
2026-05-14 16:50:41 +0900

    fix: add urls.py

--- products/urls.py
+++ products/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     
     # 목록 조회용: http://127.0.0.1:8000/api/products/
     path('products/', views.ProductListView.as_view()),
+
+    path('api/stock-chart-data/', views.stock_chart_data, name='stock_chart_data'),
 ]
